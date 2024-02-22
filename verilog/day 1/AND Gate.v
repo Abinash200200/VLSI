d5407323aa83e4b13918d6836f4fb73c85dbb57d
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-02-22 14:05:54 +0530

    Create AND Gate.v

---
+++ verilog/day 1/AND Gate.v
@@ -0,0 +1,8 @@
+module andgate(
+  input A,       // Input A
+  input B,       // Input B
+  output Y);     // Output Y
+  
+  and(Y, A, B);  // Perform logical AND operation on inputs A and B and assign the result to Y
+  
+endmodule
