de04c8c5dbda5da52607400df4164e70ddbabbb8
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-02-22 15:08:44 +0530

    Delete verilog/day 1 directory

--- verilog/day 1/OR Gate.v
+++
@@ -1,8 +0,0 @@
-module orgate(
-  input A,       // Input A
-  input B,       // Input B
-  output Y);     // Output Y
-  
-  or(Y, A, B);   // Perform logical OR operation on inputs A and B and assign the result to Y
-  
-endmodule
