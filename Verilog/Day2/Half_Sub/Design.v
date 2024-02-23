f63bb38b76a6a990a38e4264a9ea6865f366fd0b
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-02-23 11:51:01 +0530

    Update Design.v

--- Verilog/Day2/Half_Sub/Design.v
+++ Verilog/Day2/Half_Sub/Design.v
@@ -1,1 +1,14 @@
+module half_sub (
+  input a,      // Input a
+  input b,      // Input b
+  output D,     // Output difference (D)
+  output B      // Output borrow (B)
+);
+  
+  wire b1;      // Intermediate wire to store the complement of b
+  xor xor1(D, a, b); //  XOR gate to compute the difference (D) between a and b
+  not not1(b1, b); // NOT gate to compute the complement of b
+  and and1(B, a, b1);// AND gate to compute the borrow (B) using a and the complement of b (b1)
+  
+endmodule
 
