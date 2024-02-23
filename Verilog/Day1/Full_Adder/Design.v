62c70aed8a4905d9738a9a59912e44906d9b8e3b
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-02-23 10:05:24 +0530

    Update Design.v

--- Verilog/Day1/Full_Adder/Design.v
+++ Verilog/Day1/Full_Adder/Design.v
@@ -1,1 +1,13 @@
+module full_adder (
+    input A,         // Input A
+    input B,         // Input B
+    input Cin,       // Input Carry-in
+    output Sum,      // Output Sum
+    output Cout      // Output Carry-out
+);
+
+assign Sum = A ^ B ^ Cin;                    // Calculate Sum
+assign Cout = (A & B) | (B & Cin) | (A & Cin);  // Calculate Carry-out
+
+endmodule
 
