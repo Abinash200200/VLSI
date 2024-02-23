dff22e0ac5c4fceb95e77367b46091128a260e9c
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-02-23 14:26:56 +0530

    Update Testbench.v

--- Verilog/Day2/MUX (2x1)/Testbench.v
+++ Verilog/Day2/MUX (2x1)/Testbench.v
@@ -1,1 +1,61 @@
+module mux_2x1_tb;
+  reg a, b, s;        // Define reg type for inputs a, b, and select signal s
+  wire y;             // Define wire type for output y
+  
+  mux_2x1 mx(.a(a), .b(b), .s(s), .y(y));
+  
+ initial begin
+   $dumpfile("mux_2x1.vcd");  // Specify dump file name for waveform simulation
+   $dumpvars(0, mux_2x1_tb);  // Dump variables for waveform simulation
+ end
+  
+  initial begin
+    a = 1'b0;    // Assign value to input a
+    b = 1'b0;    // Assign value to input b
+    s = 1'b0;    // Assign value to input select signal s
+    #10          // Wait for 10 time units
+    
+    a=1'b0;
+    b=1'b0;
+    s=1'b1;
+    #10
+    
+    a=1'b0;
+    b=1'b1;
+    s=1'b0;
+    #10 
+    
+    a=1'b0;
+    b=1'b1;
+    s=1'b1;
+    #10 
+    
+    a=1'b1;
+    b=1'b0;
+    s=1'b0;
+    #10
+    
+    a=1'b1;
+    b=1'b0;
+    s=1'b1;
+    #10 
+    
+    a=1'b1;
+    b=1'b1;
+    s=1'b0;
+    #10 
+    
+    a=1'b1;
+    b=1'b1;
+    s=1'b1;
+    #10
+    
+    $finish;     // Finish simulation
+  end
+
+  
+  always @(*) begin
+    $monitor("$time=%0t , a=%0b , b=%0b , s=%0b , y=%0b" ,$time, a, b, s, y);  // Monitor input and output values
+  end
+endmodule
 
